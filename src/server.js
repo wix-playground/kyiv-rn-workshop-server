69dd0c434260e5c66f0440e518b7ae346893f404
Andrey Kogut <andriik@wix.com>
2019-06-06 15:25:15 +0300

    fix paths

--- src/server.js
+++ src/server.js
@@ -10,11 +10,12 @@ module.exports = (app) => {
 
   app.use('/static', express.static('static'));
 
-  app.get('/xxx', (req, res) => {
-    const baseUrl = url.resolve(
+  app.get('/', (req, res) => {
+    const toAbsolute = (...path) => [
       `${req.protocol}://${req.headers.host}`,
-      req.path,
-    );
+      req.url,
+      ...path,
+    ].map(p => p.replace(/(^\/|\/$)/g, '')).filter(Boolean).join('/');
 
     res.json({
       success: true,
@@ -28,7 +29,7 @@ module.exports = (app) => {
             'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
           message:
             'I will go back to Gotham and I will fight men Iike this but I will not become an executioner.',
-          image: `${baseUrl}/static/01.jpg`,
+          image: toAbsolute('static/01.jpg'),
         },
         {
           id: 2,
@@ -38,7 +39,7 @@ module.exports = (app) => {
             'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
           status: 'Offline',
           message: "Someone like you. Someone who'll rattle the cages.",
-          image: `${baseUrl}/static/02.jpg`,
+          image: toAbsolute('static/02.jpg'),
         },
         {
           id: 3,
@@ -49,7 +50,7 @@ module.exports = (app) => {
           status: 'Offline',
           message:
             'Oh, hee-hee, aha. Ha, ooh, hee, ha-ha, ha-ha. And I thought my jokes were bad.',
-          image: `${baseUrl}/static/03.jpg`,
+          image: toAbsolute('static/03.jpg'),
         },
         {
           id: 4,
@@ -59,7 +60,7 @@ module.exports = (app) => {
             'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
           status: 'Online',
           message: "Bats frighten me. It's time my enemies shared my dread.",
-          image: `${baseUrl}/static/04.jpg`,
+          image: toAbsolute('static/04.jpg'),
         },
         {
           id: 5,
@@ -70,7 +71,7 @@ module.exports = (app) => {
           status: 'Offline',
           message:
             "It's not who I am underneath but what I do that defines me.",
-          image: `${baseUrl}/static/05.jpg`,
+          image: toAbsolute('static/05.jpg'),
         },
         {
           id: 6,
@@ -81,7 +82,7 @@ module.exports = (app) => {
           status: 'Online',
           message:
             'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
-          image: `${baseUrl}/static/06.jpg`,
+          image: toAbsolute('static/06.jpg'),
         },
         {
           id: 7,
@@ -92,7 +93,7 @@ module.exports = (app) => {
           status: 'Online',
           message:
             "Never start with the head. The victim gets all fuzzy. He can't feel the next... See?",
-          image: `${baseUrl}/static/07.jpg`,
+          image: toAbsolute('static/07.jpg'),
         },
         {
           id: 8,
@@ -109,7 +110,7 @@ module.exports = (app) => {
           status: 'Online',
           message:
             "And as for the television's so-called plan, Batman has no jurisdiction.",
-          image: `${baseUrl}/static/08.jpg`,
+          image: toAbsolute('static/08.jpg'),
         },
         {
           id: 9,
@@ -120,7 +121,7 @@ module.exports = (app) => {
           status: 'Online',
           message:
             'This is what happens when an unstoppable force meets an immovable object.',
-          image: `${baseUrl}/static/09.jpg`,
+          image: toAbsolute('static/09.jpg'),
         },
         {
           id: 10,
@@ -131,7 +132,7 @@ module.exports = (app) => {
           status: 'Offline',
           message:
             'You want order in Gotham. Batman must take off his mask and turn himself in.',
-          image: `${baseUrl}/static/10.jpg`,
+          image: toAbsolute('static/10.jpg'),
         },
       ],
     });
